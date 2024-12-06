96064f2d2320acddc01b907fdce1a7f3251f96d7
Ninad-arakh <arakhninad@gmail.com>
2024-12-06 22:03:49 +0530

    api call testing

--- src/app.js
+++ src/app.js
@@ -2,9 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res) => {
-  res.send("hello form server!");
-});
+let obj = {
+  "firstName" : "Vijay",
+  "lastName" : "Deverkonda",
+  "age" : 33
+}
+
+app.get("/home",(req,res) => {
+  res.send(obj);
+})
+
+app.post("/home", (req, res) =>{
+  res.send("data added successfully!")
+  
+  console.log(req.query);
+})
 
 app.listen(7777, () => {
   console.log("server is running on port 7777");
