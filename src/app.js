b4f5b0761f3bab7244e8f220800e8a438903aa9d
Ninad-arakh <arakhninad@gmail.com>
2025-09-05 16:00:17 +0530

    Update app.js frontend allowed

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use('/uploads', express.static(path.join(__dirname, "..",'uploads')));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://dev-tinder-rose.vercel.app"],
     credentials: true,
   })
 );
